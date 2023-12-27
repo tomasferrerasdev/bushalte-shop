3f860909b4506024182c218e8c733bcf359d87af
Tomas Ferreras <hellotomasdev@gmail.com>
2023-12-27 18:43:10 -0300

    feat: add config folder with fonts

---
+++ src/config/fonts.ts
@@ -0,0 +1,22 @@
+import localFont from 'next/font/local';
+
+export const cascadia = localFont({
+  src: [
+    {
+      path: '../../public/fonts/cascadia/CascadiaMono-SemiLight.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/cascadia/CascadiaMono-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/cascadia/CascadiaMono-SemiBold.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+});
